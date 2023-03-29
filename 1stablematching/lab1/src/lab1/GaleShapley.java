739345ed925822d70a9bc708dac7047a55abd395
oscar.harju@gmail.com <oscar.harju@gmail.com>
2023-03-29 18:26:01 +0200

    something working maybe not

--- 1stablematching/lab1/src/lab1/GaleShapley.java
+++ 1stablematching/lab1/src/lab1/GaleShapley.java
@@ -14,8 +14,8 @@ public class GaleShapley {
 	public static void main(String[] args) {
 
 		inputParser();
-		System.out.println(Arrays.deepToString(personer));
-		System.out.println(Arrays.deepToString(företag));
+		//System.out.println(Arrays.deepToString(personer));
+		//System.out.println(Arrays.deepToString(företag));
 		galeShapleyPairing();
 
 		return;
@@ -37,13 +37,13 @@ public class GaleShapley {
 
 			for (int i = 0; i < nbrOfPairs; i++) {
 				if (hasBeenPassed) {// Lägger in för person individNbr
-					personer[individNbr - 1][i] = scan.nextInt();
+					personer[individNbr - 1][i] = scan.nextInt() -1;
 				} else {// Lägger in för företag individNbr
-					företag[individNbr - 1][i] = scan.nextInt();
+					företag[individNbr - 1][i] = scan.nextInt() -1;
 				}
 			}
 
-			// System.out.println(hasBeenPassed);
+			//System.out.println(hasBeenPassed);
 
 		}
 
@@ -53,18 +53,63 @@ public class GaleShapley {
 
 	private static void galeShapleyPairing() {
 		LinkedList<Integer> alonePersons = new LinkedList<Integer>();
-		int[][] pairing = new int[nbrOfPairs][2];
+		int[] pairing = new int[nbrOfPairs]; //Företag index
+		boolean[][] applied = new boolean[nbrOfPairs][nbrOfPairs];
 		for(int i = 0; i < nbrOfPairs; i++) {//Alla personer i en lista
 			alonePersons.add(i);
-			pairing[i][0] = -1; //Ingen är parad från början, betecknas med -1
+			pairing[i] = -1;
 		}
-		System.out.println(Arrays.deepToString(pairing));
+		//System.out.println(Arrays.deepToString(pairing));
 		while(!alonePersons.isEmpty()) {
 			int currentPerson = alonePersons.pop();
 			
-			
+			for(int i = 0; i < nbrOfPairs; i++) {
+				int companyPrioI = personer[currentPerson][i];
+				boolean current_applied = applied[currentPerson][ companyPrioI ];
+				if (current_applied) {
+					continue;
+				}
+				
+				if (pairing[companyPrioI] == -1) {
+					addPairing(pairing, companyPrioI, currentPerson);
+					applied[currentPerson][ companyPrioI ] = true;
+					break;
+				} else if(companyPrefersAOverB(i,currentPerson,pairing[i])) {
+					int previousPerson = replacePairing(pairing,i,currentPerson);
+					alonePersons.add(previousPerson);
+				} else {
+					alonePersons.add(currentPerson);
+				}
+			}
 			
 		}
+		for (int i = 0; i < nbrOfPairs; i++) {
+			System.out.println(pairing[i] +1);
+		}
 		return;
+	}
+	
+	private static void addPairing(int[] pairing,int company,int person) {
+		assert pairing[company] == -1;
+		pairing[company] = person;
+		return;
+	}
+	
+	private static int replacePairing(int[] pairing,int company,int person) {
+		int ret = pairing[company];
+		pairing[company] = person;
+		return ret;
+	}
+	
+	private static boolean companyPrefersAOverB(int company, int personA, int personB) {
+		
+		for(int i = 0; i < företag[company].length;i++) {
+			
+			if(personA == företag[company][i] || personB == företag[company][i]) {
+				return personA == företag[company][i];
+			}
+		}
+		assert false;
+		return false;
 	}
 }
