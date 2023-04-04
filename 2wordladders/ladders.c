2022a116305617997359414eae84d1f237665de6
Michal Pomorski <mi3031po-s@student.lu.se>
2023-04-04 17:11:40 +0200

    solve ladders

--- 2wordladders/ladders.c
+++ 2wordladders/ladders.c
@@ -15,11 +15,24 @@ typedef struct counter{
 
 
 typedef struct word {
-    char word[W];
+    char word[W+1];
     counter cl;
     counter ca;
 } word;
 
+
+void countChar(counter *counter, char c);
+
+counter countChars(const char in[W+1]){
+    counter count;
+    memset(&count, 0, sizeof(count) );
+    const char *p, *c = in;
+     
+    while( *(p = c++) ){
+        countChar(&count, *p);
+    }
+    return count;
+}
 
 void countChar(counter* counter, char c)
 {
@@ -41,16 +54,9 @@ word createWord(char in[100]){
     word ret;
     memset(&ret, 0, sizeof(ret) );
     strncpy( ret.word, in, LEN(ret.word) );
-
-    for (int i = 0; i<W; ++i){
-        countChar(&ret.ca, in[i]);       
-    }
-
-    for (int i = S-1; i>=0; --i){
-        countChar(&ret.cl, in[W-1-i]);       
-    }
     
-    printf("%s \n", ret.word);
+    ret.ca = countChars(in);
+    ret.cl = countChars(&in[1]);
     return ret;
 }
 
@@ -60,10 +66,27 @@ word createWord(char in[100]){
 
 typedef struct node{
     word  w;
-    struct node * edges;
+    struct node * edges[10000];
     size_t nedges;
+    int visited;
+    int depth;
 } node;
 
+typedef struct queue{
+    node** data;
+    size_t head;
+    size_t tail;
+} queue;
+
+void  queueInit(size_t size,queue* queue){
+    queue->data = calloc(size,sizeof(node*));
+    queue->head = 0;
+    queue->tail = 0;
+}
+
+void queuePush(queue *queue, node *node) { queue->data[queue->tail++] = node; }
+
+node *queuePop(queue *queue) { return queue->data[queue->head++]; }
 
 int findInCounter(const counter* counter, char c){
     for (int i = 0; i<LEN(counter->chars); ++i){
@@ -74,30 +97,92 @@ int findInCounter(const counter* counter, char c){
     return 0;
 }
 
-bool hasEdgeFromAToB(const node* A, const node* B){
+
+
+
+int hasEdgeFromAToB(const node* A, const node* B){
     const char *c, *p = A->w.cl.chars;
-    
-    while ( !(p = c++) ) {
-        int n = findInCounter(&B->w.ca, *p);
+
+    for (int i = 0; i<LEN(A->w.cl.chars); ++i){
+
+        char cA = A->w.cl.chars[i];
+        if (!cA){ //compared all characters 
+            break; 
+        }
         
-
-    };
-
-
+        int cntA =  A->w.cl.counts[i];
+        int cntB = findInCounter(&B->w.ca, cA);
+        if (cntB < cntA){
+            
+            return 0;
+        }
+    }
     
+    return 1;
+}
+
+void addEdge(node *from,  node *to) {
+    from->edges[from->nedges++] = to;
 }
 
 void addEdges(node* nodes, size_t currentIndex){
     word current = nodes[currentIndex].w;
     for (int i = 0; i<currentIndex; ++i){
         //        nodes[i].edges[]
+        if(hasEdgeFromAToB(&nodes[currentIndex], &nodes[i])){
+            addEdge(&nodes[currentIndex],&nodes[i]);
+        }
+        if(hasEdgeFromAToB(&nodes[i], &nodes[currentIndex])){
+            addEdge(&nodes[i], &nodes[currentIndex]);
+        }
 
     }
 }
 
+node* findNode(node* nodes, const char* word, int size){
+    for(int i = 0; i < size;i++){
+        if(!strcmp( nodes[i].w.word, word)){
+            return &nodes[i];
+        }
+    }
+    return 0;
+}
+
+int bFS(const node* nodes,int size,  node* sNode, const node* tNode){
+    if(sNode == tNode){return 0;}
+    queue Q;
+    queueInit(size, &Q);
+    queuePush(&Q, sNode);
+
+    sNode->visited = 1;
+    sNode->depth = 0;
+
+    //while queue is not empty
+    while(Q.head != Q.tail){
+        node* v = queuePop(&Q);
+        
+        for(int i = 0; i < v->nedges;i++){
+            node * w = v->edges[i];
+
+            if(!w->visited){
+
+                w->visited = 1;
+                w->depth = v->depth + 1;
+
+                queuePush(&Q,w);
+
+                if(w == tNode){
+                    return w->depth;
+                }
+            }
+        }
+    }
+    return -1;
+}
+
 int main()
 {
-    char in[100]; // declare a char array to hold the word
+    char in[6]; // declare a char array to hold the word
     node* nodes;
     int N;
     int Q;
@@ -106,10 +191,33 @@ int main()
     nodes = malloc(sizeof(node) *N);
     int wordN = 0;
 
-    while(scanf("%99s", in) == 1) { // read in each word separated by spaces
-        nodes[wordN++].w =createWord(in);
+
+    for (int i = 0; i < N; i++) {
+        scanf("%5s", in); // read in each word separated by spaces
+        nodes[wordN].w =createWord(in);
+        nodes[wordN].nedges = 0;
         addEdges(nodes, wordN);
+        wordN++;
     }
+    
+    for (int i = 0; i < Q; i++) {
+        
+        char request1[6];
+        char request2[6];
+        scanf("%5s", request1); // read in each word separated by spaces
+        scanf("%5s", request2); // read in each word separated by spaces
+        node* node1 = findNode(nodes,request1,N);
+        const node* node2 = findNode(nodes,request2,N);
+        for (int i = 0; i < N; ++i){
+            nodes[i].visited = 0;
+        }
+        int nbrSteps = bFS(nodes,N,node1,node2); //Breadth first search
+        nbrSteps < 0 ? printf("Impossible\n") : printf("%d\n", nbrSteps);
+        //printf("Steps from %s ro %s: %d", request1, request2, nbrSteps);
+    }
+       
+        
+ 
     return 0;
 }
     
