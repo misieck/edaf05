4bb301bb8acf2b1ef6e08c3b42bf95bac13cb264
Michal Pomorski <mi3031po-s@student.lu.se>
2023-04-05 13:41:09 +0200

    add comments

--- 2wordladders/ladders.c
+++ 2wordladders/ladders.c
@@ -8,7 +8,7 @@
 #define S 4
 #define LEN(arr) (sizeof(arr) / sizeof(arr[0]))
 #define MAXEDGES 8000
-#define INSIZE 6
+#define INSIZE (W+1)
 #define INFMT "%5s"
 
 typedef struct counter{
@@ -23,15 +23,9 @@ typedef struct node{
     struct node * edges[MAXEDGES];
     size_t nedges;
     int visited;
-    int depth;
+    int depth; //if all you want is the shortest length
+    //struct node * pred; //if you want the actual path found, add w->pred = v; in bfs
 } node;
-
-typedef struct queue{
-    node** data;
-    size_t head;
-    size_t tail;
-} queue;
-
 
 void countChar(counter *counter, char c);
 
@@ -62,7 +56,16 @@ void countChar(counter* counter, char c)
     }
 }
 
-void  queueInit(size_t size,queue* queue){
+// We will need at most the number of nodes elements in the queue, and each
+// element is only added once
+// So no size checks are necessary, simply allocate enough for bfs to work.
+typedef struct queue{
+    node** data;
+    size_t head;
+    size_t tail;
+} queue;
+
+void  queueInit(size_t size, queue* queue){
     queue->data = calloc(size,sizeof(node*));
     queue->head = 0;
     queue->tail = 0;
@@ -72,9 +75,13 @@ void  queueFree(queue* queue){
     free(queue->data);
 }
 
-void queuePush(queue *queue, node *node) { queue->data[queue->tail++] = node; }
+void queuePush(queue *queue, node *node) {
+    queue->data[queue->tail++] = node;
+}
 
-node *queuePop(queue *queue) { return queue->data[queue->head++]; }
+node *queuePop(queue *queue) {
+    return queue->data[queue->head++];
+}
 
 int findInCounter(const counter* counter, char c){
     for (int i = 0; i<LEN(counter->chars); ++i){
@@ -114,7 +121,7 @@ void addEdges(node* nodes, size_t currentIndex){
     for (int i = 0; i<currentIndex; ++i){
         //        nodes[i].edges[]
         if(hasEdgeFromAToB(&nodes[currentIndex], &nodes[i])){
-            addEdge(&nodes[currentIndex],&nodes[i]);
+            addEdge(&nodes[currentIndex], &nodes[i]);
         }
         if(hasEdgeFromAToB(&nodes[i], &nodes[currentIndex])){
             addEdge(&nodes[i], &nodes[currentIndex]);
@@ -132,18 +139,20 @@ node* findNode(node* nodes, const char* word, int size){
     return 0;
 }
 
-int bFS(const node* nodes,int size,  node* sNode, const node* tNode){
+int bFS(int size,  node* sNode, const node* tNode){
     if(sNode == tNode){return 0;}
     queue Q;
-    queueInit(size, &Q);
-    queuePush(&Q, sNode);
+
+    //the maximum size of the queue required is at most the number of graph nodes
+    queueInit( size, &Q );
+    queuePush( &Q, sNode );
 
     sNode->visited = 1;
     sNode->depth = 0;
 
     //while queue is not empty
-    while(Q.head != Q.tail){
-        node* v = queuePop(&Q);
+    while( Q.head != Q.tail ){
+        node* v = queuePop( &Q );
         
         for(int i = 0; i < v->nedges;i++){
             node * w = v->edges[i];
@@ -153,53 +162,58 @@ int bFS(const node* nodes,int size,  node* sNode, const node* tNode){
                 w->visited = 1;
                 w->depth = v->depth + 1;
 
-                queuePush(&Q,w);
+                queuePush( &Q, w );
 
                 if(w == tNode){
-                    queueFree(&Q);
+                    queueFree( &Q );
+                    //because bfs traverses full "levels" at a time, before going deeper,
+                    //the first path found must be the shortest
                     return w->depth;
                 }
             }
         }
     }
-    queueFree(&Q);
+    queueFree( &Q );
     return -1;
 }
 
 int main()
 {
-    char in[INSIZE]; // declare a char array to hold the word
+    char in[INSIZE]; // declare a char array to hold the input word
     node* nodes;
     int N;
     int Q;
-    scanf("%d", &N);
-    scanf("%d", &Q);
+    int err;
+    err = scanf("%d", &N);
+    err = scanf("%d", &Q);
     nodes = malloc(sizeof(node) *N);
     int wordN = 0;
 
-
+    size_t SUFFIX_OFFSET = W-S; 
+    
+    //read in N words and build the graph.
     for (int i = 0; i < N; i++) {
-        scanf(INFMT, in); // read in each word separated by spaces
+        err = scanf(INFMT, in); // read in each word separated by spaces
         strncpy( nodes[wordN].word, in, LEN(nodes[0].word) );
-        nodes[wordN].ca = countChars(in);
-        nodes[wordN].cl = countChars(&in[1]);
+        nodes[wordN].ca = countChars( in );
+        nodes[wordN].cl = countChars( &in[SUFFIX_OFFSET] );
         nodes[wordN].nedges = 0;
         addEdges(nodes, wordN);
         wordN++;
     }
-    
+
+    //read in Q requests and solve them using bfs
     for (int i = 0; i < Q; i++) {
-        
         char request1[INSIZE];
         char request2[INSIZE];
-        scanf(INFMT, request1); // read in each word separated by spaces
-        scanf(INFMT, request2); // read in each word separated by spaces
+        err = scanf(INFMT, request1); 
+        err = scanf(INFMT, request2); 
         node* node1 = findNode(nodes,request1,N);
         const node* node2 = findNode(nodes,request2,N);
         for (int i = 0; i < N; ++i){
             nodes[i].visited = 0;
         }
-        int nbrSteps = bFS(nodes,N,node1,node2); //Breadth first search
+        int nbrSteps = bFS(N,node1,node2); //Breadth first search
         nbrSteps < 0 ? printf("Impossible\n") : printf("%d\n", nbrSteps);
         //printf("Steps from %s ro %s: %d", request1, request2, nbrSteps);
     }
